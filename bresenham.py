117e8a89c79b0e07a4e683ad318c47ba3a988c7f
Vinícius Botelho <68855615+viniciusdev-br@users.noreply.github.com>
2023-05-29 00:21:00 -0300

    add: Adiciona algorítmo do circulo

--- bresenham.py
+++ bresenham.py
@@ -34,8 +34,38 @@ def bresenham_line(selected_cells, rendered_cells, parameters):
     for c in points:
 	    grid.render_cell(c)
 
+def ponto_medio(selected_cells, rendered_cells, parameters):
+    r = 5
+    coordenadas_circulo = []
+    def desenha8(x, y, xc, yc):
+        coordenadas_circulo.append((x + xc, y + yc))
+        coordenadas_circulo.append((y + xc, x + yc))
+        coordenadas_circulo.append((y + xc, -x + yc))
+        coordenadas_circulo.append((x + xc, -y + yc))
+        coordenadas_circulo.append((-x + xc, -y + yc))
+        coordenadas_circulo.append((-y + xc, -x + yc))
+        coordenadas_circulo.append((-y + xc, x + yc))
+        coordenadas_circulo.append((-x + xc, y + yc))
+    xc = 0 
+    yc = 0 
+    x = 0 
+    y = r
+    e = -r
+    desenha8(x, y, xc, yc)    
+    while (x <= y):
+        e += 2*x + 1
+        x += 1
+        if (e >= 0):
+            e += 2 - 2*y
+            y -= 1
+        desenha8(x, y, xc, yc)
+    print(coordenadas_circulo)
+    for c in coordenadas_circulo:
+	    grid.render_cell(c)
+
 # Adds the algorithm to the grid
 grid.add_algorithm(name="Render cells", parameters=None, algorithm=my_render_cells_algorithm)
-grid.add_algorithm(name='Bresenham',    parameters=None, algorithm=bresenham_line)
+grid.add_algorithm(name='Bresenham', parameters=None, algorithm=bresenham_line)
+grid.add_algorithm(name='Circulo', parameters=None, algorithm=ponto_medio)
 
 grid.show()
